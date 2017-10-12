21d79c27cc4c3c504b8c60d22c80960288180f8a
odesberga <johan.hahne.83@gmail.com>
2017-10-12 22:33:51 +0200

    added menusystem

--- src/shunstyrning.ino
+++ src/shunstyrning.ino
@@ -11,6 +11,8 @@
 #define D5_pin 5
 #define D6_pin 6
 #define D7_pin 7
+unsigned long btnDelay;
+const int del=100;
 
 float val=0;
 float val2=0;
@@ -18,6 +20,30 @@ int currview=0;
 LiquidCrystal_I2C lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
 float factor1=1.0;
 float factor2=1.0;
+int pc=3;
+int ports[3][4] = {
+  {7, 0, 0, 0} ,
+  {8, 0, 0, 0} ,
+  {9, 0, 0, 0} };
+
+  String submenuItem1values[2] = {
+    {"Ja"} ,
+    {"Nej"} };
+
+
+
+  bool insubmenu=false;
+  int currentmenuitem=0;
+  int menuitems=4;
+  int currentsubmenuitem=0;
+  int submenuItemscount=0;
+  String menuItemvalues[4] = {
+    {""} ,
+    {""} ,
+    {""} ,
+    {""} };
+
+
 
 void setup()
 {
@@ -32,18 +58,21 @@ lcd.clear() ; // go home on LCD
 lcd.print("johan");
 lcd.setCursor(5, 1);
 lcd.print("hahne");
+for(int i = 0;i<pc;i++){
+pinMode(ports[i][0], OUTPUT);
+digitalWrite(ports[i][0],HIGH);}
 }
 
 
 void loop()
 {
 
-  val=analogRead(A3);
-  val2=analogRead(A4);
+  val=analogRead(14);
+  val2=analogRead(15);
 setFactors();
-    showview(1);
-delay(1000);
 
+
+readButtons();
 }
 
 void showview(int viewnr)
@@ -61,7 +90,7 @@ void showview(int viewnr)
 
 void MainView(int outtemp,float pipetemp)
 {
-//    lcd.clear();
+    lcd.clear();
     String outstr ="Ute temp:"+(String)outtemp+";C";
     Printstring(0,0,outstr);
      outstr ="Shunt temp:"+(String)pipetemp+";C";
@@ -70,7 +99,7 @@ void MainView(int outtemp,float pipetemp)
 }
 void TempView()
 {
-//    lcd.clear();
+    lcd.clear();
     String outstr ="U/F 20/"+(String)(int)calcTemp(20)+" 10/"+(String)(int)calcTemp(10)+" 0/"+(String)(int)calcTemp(0);
 
     Printstring(0,0,outstr);
@@ -100,8 +129,11 @@ void Printstring(int row,int index,String outstring)
 
 void setFactors()
 {
-    factor1=((val/1022)+0.8)*1.3;
-    factor2=(val2/1022)+1;
+//    factor1=((val/1022)+0.8)*1.3;
+//    factor2=(val2/1022)+1;
+
+factor1=val;
+factor2=val2;
 }
 
 float calcTemp(int outTemp)
@@ -120,5 +152,172 @@ return 99;
 } else {
 return v;
 }
+
+}
+
+void readButtons() {
+ if (millis()-btnDelay >= del) {
+for(int i = 0;i<pc;i++){
+    if(digitalRead(ports[i][0]) == LOW){
+     ports[i][1]=1;
+    }
+    else
+    {
+      ports[i][1]=0;
+    }
+
+    if(ports[i][1]!=ports[i][2]){
+      if(ports[i][1]==0){
+       ports[i][2]=0;
+
+
+      }
+      if(ports[i][1]==1){
+           ports[i][2]=1;
+          buttonpush(i);
+    }
+
+
+    }
+}
+btnDelay=millis();
+}
+
+
+}
+
+void buttonpush(int button){
+      switch (button) {
+        case 0: Prevbutton();
+          break;
+        case 1: OkButton();
+            break;
+        case 2: Nexbutton();
+                break;
+      }
+}
+void Prevbutton(){
+  if (!insubmenu){
+  if (currentmenuitem==0){
+    currentmenuitem=menuitems;
+  } else {
+    currentmenuitem--;
+  }
+  showmenuitem(currentmenuitem);
+
+  } else {
+  if (currentsubmenuitem==0){
+    currentsubmenuitem=submenuItemscount-1;
+  } else {
+    currentsubmenuitem--;
+  }
+showsubmenuitem(currentmenuitem,currentsubmenuitem);
+  }
+}
+
+
+
+void Nexbutton(){
+  if (!insubmenu){
+  if (currentmenuitem==menuitems){
+    currentmenuitem=0;
+  } else {
+    currentmenuitem++;
+  }
+  showmenuitem(currentmenuitem);
+
+} else {
+  if (currentsubmenuitem==submenuItemscount-1){
+    currentsubmenuitem=0;
+  } else {
+    currentsubmenuitem++;
+
+  }
+showsubmenuitem(currentmenuitem,currentsubmenuitem);
+}
+
+}
+
+
+void OkButton()
+{
+if (!insubmenu){
+  insubmenu=true;
+  showsubmenuitem(currentmenuitem,0);
+} else {
+  insubmenu=false;
+  showmenuitem(currentmenuitem);
+}
+
+}
+
+void showmenuitem(int item){
+  switch(item) {
+    case 0: MainView(1,1);
+    break;
+    case 1: menuItem1();
+    break;
+    case 2: menuItem2();
+    break;
+  }
+}
+
+void showsubmenuitem(int menuitem,int submenuitem){
+    switch(menuitem) {
+      case 0: MainView(1,1);
+      break;
+      case 1: submenuItem1(submenuitem);
+      break;
+      case 2: submenuItem2(submenuitem);
+      break;
+    }
+
+}
+
+void menuItem1()
+{
+    lcd.clear();
+    currentsubmenuitem=0;
+    submenuItemscount=2;
+    String outstr ="Kontrollera cirkpump";
+    Printstring(0,0,outstr);
+     outstr =menuItemvalues[1];
+    Printstring(1,0,outstr);
+
+}
+void submenuItem1(int item)
+{
+    lcd.clear();
+    String outstr ="ja/nej";
+    Printstring(0,0,outstr);
+    outstr =submenuItem1values[item];
+    menuItemvalues[1]=submenuItem1values[item];
+    Printstring(1,0,outstr);
+
+}
+
+
+void menuItem2()
+{
+    lcd.clear();
+    String outstr ="Stoppa pump vid temp";
+    currentsubmenuitem=0;
+    submenuItemscount=20;
+    Printstring(0,0,outstr);
+     outstr =menuItemvalues[2];
+    Printstring(1,0,outstr);
+    lcd.print((char)223);
+    lcd.print("C");
+
+
+}
+void submenuItem2(int item)
+{
+    lcd.clear();
+    String outstr ="Ange grader:";
+    Printstring(0,0,outstr);
+    outstr =(String)item;
+    menuItemvalues[2]=(String)item;
+    Printstring(1,0,outstr);
 
 }
