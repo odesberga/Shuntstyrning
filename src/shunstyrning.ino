00e7c77d9e1c64f2d9c9844661cb200b449a7d7e
odesberga <johan.hahne.83@gmail.com>
2017-10-14 16:14:50 +0200

    added shunt functions

--- src/shunstyrning.ino
+++ src/shunstyrning.ino
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+#include <EEPROM.h>
 #include "Wire.h"
 #include "LCD.h"
 #include "LiquidCrystal_I2C.h"
@@ -11,69 +13,192 @@
 #define D5_pin 5
 #define D6_pin 6
 #define D7_pin 7
-unsigned long btnDelay;
 const int del=100;
-
+const int menuitemCount=4;
+const int pc=3; //portcount
+unsigned long ScreentimeOut=0;
+unsigned long ShunttimeOut=0;
+unsigned long buttoninactive=0;
+unsigned long shuntdelay=0;
+unsigned long btnDelay;
+unsigned long gendel;
 float val=0;
 float val2=0;
 int currview=0;
 LiquidCrystal_I2C lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
 float factor1=1.0;
 float factor2=1.0;
-int pc=3;
-int ports[3][4] = {
+float lastfactor=1;
+
+int ports[pc][4] = {
   {7, 0, 0, 0} ,
   {8, 0, 0, 0} ,
   {9, 0, 0, 0} };
 
-  String submenuItem1values[2] = {
-    {"Ja"} ,
-    {"Nej"} };
+  struct ssubmenuitem {
+      String row1="";
+      String row2="";
+      String row3="";
+      String row4="";
+      int minvalue=0;
+      int maxvalue=0;
+      String submenuStringvalues[2];
+  };
+
+struct smenuitem {
+    String row1=""; //callfunc name
+    String row2="";
+    String row3="";
+    String row4="";
+    int value=0;
+    bool useStringvalue=false;
+    bool callFunc=false;
+    ssubmenuitem smenitem;
+};
 
 
+ smenuitem menuItems[menuitemCount];
 
+  bool inMainView=false;
   bool insubmenu=false;
   int currentmenuitem=0;
-  int menuitems=4;
   int currentsubmenuitem=0;
   int submenuItemscount=0;
-  String menuItemvalues[4] = {
-    {""} ,
-    {""} ,
-    {""} ,
-    {""} };
 
 
-
+bool firstrun=true;
+int pdelay=200;
 void setup()
 {
-
- lcd.begin (20,4); //  our LCD is a 20x4, change for your LCD if needed
-
-// LCD Backlight ON
+defMenu();
+Serial.begin(9600);
+lcd.begin (20,4);
 lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
 lcd.setBacklight(HIGH);
 
 lcd.clear() ; // go home on LCD
-lcd.print("johan");
-lcd.setCursor(5, 1);
-lcd.print("hahne");
+lcd.setCursor(0, 0);
+lcd.print("####################");
+lcd.setCursor(0, 1);
+lcd.print("# Utvecklad 2017   #");
+lcd.setCursor(0, 2);
+lcd.print("# Johan Hahne      #");
+lcd.setCursor(0, 3);
+lcd.print("####################");
+
+
+lcd.print("");
 for(int i = 0;i<pc;i++){
 pinMode(ports[i][0], OUTPUT);
 digitalWrite(ports[i][0],HIGH);}
+buttoninactive=millis();
+gendel=millis();
 }
 
 
 void loop()
 {
 
-  val=analogRead(14);
-  val2=analogRead(15);
+if (millis()-gendel >= pdelay) {
+
+  val=analogRead(A0);
+  val2=analogRead(A1);
 setFactors();
-
-
 readButtons();
+
+
+if ( millis() - buttoninactive >= 5000 ){
+    if (!insubmenu){
+        if (firstrun){
+        lcd.clear();
+        firstrun=false;
+        }
+
+
+    TempView();
+    }
 }
+if ( millis() - ScreentimeOut >= 10000 ){
+    if (!insubmenu){
+    lcd.setBacklight(LOW);
+    }
+}
+
+}
+
+}
+
+
+void defMenu(){
+    //meny 1
+    int m =0;
+    menuItems[m].row1="Kontrollera cirkpump";
+    menuItems[m].row2="@";
+    menuItems[m].useStringvalue=true;
+    menuItems[m].smenitem.maxvalue=1;
+    menuItems[m].smenitem.minvalue=0;
+    menuItems[m].smenitem.row1="Ange Ja/Nej";
+    menuItems[m].smenitem.row2="@";
+    menuItems[m].smenitem.submenuStringvalues[0]="Ja";
+    menuItems[m].smenitem.submenuStringvalues[1]="Nej";
+    if( EEPROM.read(m) > menuItems[m].smenitem.maxvalue){
+    menuItems[m].value= menuItems[m].smenitem.minvalue;
+    } else{
+    menuItems[m].value= EEPROM.read(m);
+    }
+    //meny2
+    m =1;
+    menuItems[m].row1="Stoppa pump vid:";
+    menuItems[m].row2="@ ;C";
+    menuItems[m].useStringvalue=false;
+    menuItems[m].smenitem.maxvalue=30;
+    menuItems[m].smenitem.minvalue=0;
+    menuItems[m].smenitem.row1="Ange ;C";
+    menuItems[m].smenitem.row2="@";
+    menuItems[m].smenitem.submenuStringvalues[0]="";
+    menuItems[m].smenitem.submenuStringvalues[1]="";
+    if( EEPROM.read(m) > menuItems[m].smenitem.maxvalue){
+    menuItems[m].value= menuItems[m].smenitem.minvalue;
+    } else{
+    menuItems[m].value= EEPROM.read(m);
+    }
+    //meny3
+    m =2;
+    menuItems[m].row1="Shunt intervall:";
+    menuItems[m].row2="@ Sek";
+    menuItems[m].useStringvalue=false;
+    menuItems[m].smenitem.maxvalue=30;
+    menuItems[m].smenitem.minvalue=0;
+    menuItems[m].smenitem.row1="Ange Sek";
+    menuItems[m].smenitem.row2="@";
+    menuItems[m].smenitem.submenuStringvalues[0]="";
+    menuItems[m].smenitem.submenuStringvalues[1]="";
+    if( EEPROM.read(m) > menuItems[m].smenitem.maxvalue){
+    menuItems[m].value= menuItems[m].smenitem.minvalue;
+    } else{
+    menuItems[m].value= EEPROM.read(m);
+    }
+    m =3;
+    menuItems[m].row1="Shuntmotor sekunder";
+    menuItems[m].row2="Min -> Max";
+    menuItems[m].row3="@ Sek";
+    menuItems[m].useStringvalue=false;
+    menuItems[m].smenitem.maxvalue=150;
+    menuItems[m].smenitem.minvalue=80;
+    menuItems[m].smenitem.row1="Ange Sek";
+    menuItems[m].smenitem.row2="@";
+    menuItems[m].smenitem.submenuStringvalues[0]="";
+    menuItems[m].smenitem.submenuStringvalues[1]="";
+    if( EEPROM.read(m) > menuItems[m].smenitem.maxvalue){
+    menuItems[m].value= menuItems[m].smenitem.minvalue;
+    } else{
+    menuItems[m].value= EEPROM.read(m);
+    }
+
+}
+
+
+
 
 void showview(int viewnr)
 {
@@ -99,17 +224,33 @@ void MainView(int outtemp,float pipetemp)
 }
 void TempView()
 {
-    lcd.clear();
+    //lcd.clear();
+    inMainView=true;
+    String outstr ="Temp Ute:Framledning";
+    Printstring(0,0,outstr);
+
+     outstr ="20;:"+(String)(int)calcTemp(20)+"; I "+"-10;:"+(String)(int)calcTemp(-10)+";";
+    Printstring(1,0,outstr);
+
+
+     outstr ="10;:"+(String)(int)calcTemp(10)+"; I "+"-20;:"+(String)(int)calcTemp(-20)+";";
+
+Printstring(2,0,outstr);
+     outstr =" 0;:"+(String)(int)calcTemp(0)+"; I "+"-30;:"+(String)(int)calcTemp(-30)+";";
+Printstring(3,0,outstr);
+
+
+/*
     String outstr ="U/F 20/"+(String)(int)calcTemp(20)+" 10/"+(String)(int)calcTemp(10)+" 0/"+(String)(int)calcTemp(0);
 
-    Printstring(0,0,outstr);
-     outstr ="-10/"+(String)(int)calcTemp(-10)+" -20/"+(String)(int)calcTemp(-20)+" -30/"+(String)(int)calcTemp(-30);
-    Printstring(1,0,outstr);
-     outstr ="Factor1: "+(String)factor1;
-    Printstring(2,0,outstr);
-    outstr ="Factor2: "+(String)factor2;
-    Printstring(3,0,outstr);
 
+     +" -20/"+(String)(int)calcTemp(-20)+" -30/"+(String)(int)calcTemp(-30);
+    Printstring(1,0,outstr);
+     outstr ="F1: "+(String)factor1 + "   F2: "+(String)factor2;
+    Printstring(3,0,outstr);
+    //outstr ="";
+    //Printstring(3,0,outstr);
+*/
 }
 void Printstring(int row,int index,String outstring)
 {
@@ -120,31 +261,49 @@ void Printstring(int row,int index,String outstring)
         if (outstring[i] == ';')
         {
             lcd.print((char)223);
-        } else {
-        lcd.write(outstring[i]);
-        }
 
+        } else {
+            if (outstring[i] == '@'){
+                if (menuItems[currentmenuitem].useStringvalue){
+                    lcd.print(menuItems[currentmenuitem].smenitem.submenuStringvalues[menuItems[currentmenuitem].value]);
+                    Serial.print(menuItems[currentmenuitem].smenitem.submenuStringvalues[menuItems[currentmenuitem].value]);
+                } else {
+                    lcd.print(menuItems[currentmenuitem].value);
+                }
+            } else {
+                lcd.write(outstring[i]);
+            }
+        }
     }
   }
 
 void setFactors()
 {
-//    factor1=((val/1022)+0.8)*1.3;
-//    factor2=(val2/1022)+1;
+    factor1=((val/1022)+1);
+    factor2=(val2/1022)+0.5+(val/1022);
+    if(!(((int)(val+val2) > lastfactor-3) && ((int)(val+val2) < lastfactor+3)) ){
+        ScreentimeOut=millis();
+        lcd.setBacklight(HIGH);
+    }
 
-factor1=val;
-factor2=val2;
+    lastfactor = (int)(val+val2);
+
 }
 
 float calcTemp(int outTemp)
 {
-float c = 0;
+float c = 10;
 float v =0;
+
+c=c*factor1;
 for(int i=20;i > -31; i-- ){
-if(i==outTemp){
-  v=(i+(c*factor1))*factor2;
-  break;
-  }
+    if(i==outTemp){
+        v=(c*factor2);//*factor2;
+
+            //v=(c*factor1)*factor2;
+    break;
+    }
+
   c++;
 }
 if(v > 99){
@@ -169,12 +328,13 @@ for(int i = 0;i<pc;i++){
     if(ports[i][1]!=ports[i][2]){
       if(ports[i][1]==0){
        ports[i][2]=0;
-
-
       }
       if(ports[i][1]==1){
            ports[i][2]=1;
+
           buttonpush(i);
+         // buttoninactive=millis();
+          //Serial.print(buttoninactive);
     }
 
 
@@ -182,32 +342,42 @@ for(int i = 0;i<pc;i++){
 }
 btnDelay=millis();
 }
+}
 
+void runSHunt (){
+    if(millis() - shuntdelay >= menuItems[2].value){
+
+    }
+shuntdelay = millis();
 
 }
 
 void buttonpush(int button){
+    lcd.setBacklight(HIGH);
+    ScreentimeOut=millis();
       switch (button) {
         case 0: Prevbutton();
           break;
         case 1: OkButton();
             break;
         case 2: Nexbutton();
-                break;
+            break;
       }
+
 }
 void Prevbutton(){
+    buttoninactive=millis();
   if (!insubmenu){
   if (currentmenuitem==0){
-    currentmenuitem=menuitems;
+    currentmenuitem=menuitemCount-1;
   } else {
     currentmenuitem--;
   }
   showmenuitem(currentmenuitem);
 
   } else {
-  if (currentsubmenuitem==0){
-    currentsubmenuitem=submenuItemscount-1;
+  if (currentsubmenuitem==menuItems[currentmenuitem].smenitem.minvalue){
+    currentsubmenuitem=menuItems[currentmenuitem].smenitem.maxvalue;
   } else {
     currentsubmenuitem--;
   }
@@ -218,8 +388,9 @@ showsubmenuitem(currentmenuitem,currentsubmenuitem);
 
 
 void Nexbutton(){
+    buttoninactive=millis();
   if (!insubmenu){
-  if (currentmenuitem==menuitems){
+  if (currentmenuitem==menuitemCount-1){
     currentmenuitem=0;
   } else {
     currentmenuitem++;
@@ -227,8 +398,8 @@ void Nexbutton(){
   showmenuitem(currentmenuitem);
 
 } else {
-  if (currentsubmenuitem==submenuItemscount-1){
-    currentsubmenuitem=0;
+  if (currentsubmenuitem==menuItems[currentmenuitem].smenitem.maxvalue){
+    currentsubmenuitem=menuItems[currentmenuitem].smenitem.minvalue;
   } else {
     currentsubmenuitem++;
 
@@ -241,83 +412,39 @@ showsubmenuitem(currentmenuitem,currentsubmenuitem);
 
 void OkButton()
 {
-if (!insubmenu){
-  insubmenu=true;
-  showsubmenuitem(currentmenuitem,0);
-} else {
-  insubmenu=false;
-  showmenuitem(currentmenuitem);
-}
 
+if (!inMainView){
+    if (!insubmenu){
+        currentsubmenuitem=menuItems[currentmenuitem].value;
+      showsubmenuitem(currentmenuitem,currentsubmenuitem);
+    } else {
+        EEPROM.write(currentmenuitem, menuItems[currentmenuitem].value);
+      insubmenu=false;
+      showmenuitem(currentmenuitem);
+    }
+    buttoninactive=millis();
+    }
 }
 
 void showmenuitem(int item){
-  switch(item) {
-    case 0: MainView(1,1);
-    break;
-    case 1: menuItem1();
-    break;
-    case 2: menuItem2();
-    break;
-  }
+lcd.clear();
+inMainView=false;
+Printstring(0, 0, menuItems[currentmenuitem].row1);
+Printstring(1, 0, menuItems[currentmenuitem].row2);
+Printstring(2, 0, menuItems[currentmenuitem].row3);
+Printstring(3, 0, menuItems[currentmenuitem].row4);
+
 }
 
 void showsubmenuitem(int menuitem,int submenuitem){
-    switch(menuitem) {
-      case 0: MainView(1,1);
-      break;
-      case 1: submenuItem1(submenuitem);
-      break;
-      case 2: submenuItem2(submenuitem);
-      break;
-    }
-
-}
-
-void menuItem1()
-{
+    insubmenu=true;
     lcd.clear();
-    currentsubmenuitem=0;
-    submenuItemscount=2;
-    String outstr ="Kontrollera cirkpump";
-    Printstring(0,0,outstr);
-     outstr =menuItemvalues[1];
-    Printstring(1,0,outstr);
+    menuItems[currentmenuitem].value=submenuitem;
 
-}
-void submenuItem1(int item)
-{
-    lcd.clear();
-    String outstr ="ja/nej";
-    Printstring(0,0,outstr);
-    outstr =submenuItem1values[item];
-    menuItemvalues[1]=submenuItem1values[item];
-    Printstring(1,0,outstr);
+    Printstring(0, 0, menuItems[currentmenuitem].smenitem.row1);
+    Printstring(1, 0, menuItems[currentmenuitem].smenitem.row2);
+    Printstring(2, 0, menuItems[currentmenuitem].smenitem.row3);
+    Printstring(3, 0, menuItems[currentmenuitem].smenitem.row4);
 
-}
-
-
-void menuItem2()
-{
-    lcd.clear();
-    String outstr ="Stoppa pump vid temp";
-    currentsubmenuitem=0;
-    submenuItemscount=20;
-    Printstring(0,0,outstr);
-     outstr =menuItemvalues[2];
-    Printstring(1,0,outstr);
-    lcd.print((char)223);
-    lcd.print("C");
-
-
-}
-void submenuItem2(int item)
-{
-    lcd.clear();
-    String outstr ="Ange grader:";
-    Printstring(0,0,outstr);
-    outstr =(String)item;
-    menuItemvalues[2]=(String)item;
-    Printstring(1,0,outstr);
 
 }
