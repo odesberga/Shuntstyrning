0519bfb9ed9b15eec25a8544baa871d0729bf429
odesberga <johan.hahne.83@gmail.com>
2017-10-24 09:44:41 +0200

    cleanup

--- src/shunstyrning.ino
+++ src/shunstyrning.ino
@@ -28,7 +28,6 @@ const char m5r1[]  = "Shuntmotor antal";
 const char m5r2[]  = "sekunder/justering";
 const char m5r3[]  = "@ Sek";
 const char m5r4[]  = "";
-
 //submenu
 const char sm1r1[]  = "Ange Ja/Nej";
 const char sm1r2[]  = "@";
@@ -38,22 +37,20 @@ const char sm2r1[]  = "Ange ;C";
 const char sm2r2[]  = "@ ;C";
 const char sm2r3[]  = "";
 const char sm2r4[]  = "";
-const char sm3r1[]  = "Ange Sekunder";
+const char sm3r1[]  = "Ange antal";
 const char sm3r2[]  = "@ Sek";
 const char sm3r3[]  = "";
 const char sm3r4[]  = "";
-const char sm4r1[]  = "Ange Sekunder";
+const char sm4r1[]  = "Ange antal";
 const char sm4r2[]  = "Min -> Max";
 const char sm4r3[]  = "@ Sek";
 const char sm4r4[]  = "";
-const char sm5r1[]  = "Ange Sekunder";
+const char sm5r1[]  = "Ange antal";
 const char sm5r2[]  = "sekunder/justering";
 const char sm5r3[]  = "@ Sek";
 const char sm5r4[]  = "";
-
-
-
-
+String outstr ="";
+const char emptyRow   = "                   ";
 
 const char* const menu_strings[][4]  =	   // change "string_table" name to suit
 {
@@ -72,8 +69,6 @@ const char* const sub_menu_strings[][4]  =
 {sm4r1,sm4r2,sm4r3,sm4r4},
 {sm5r1,sm5r2,sm5r3,sm5r4},
 };
-
-
 
 struct ssubmenuitem {
     int minvalue=0;
@@ -101,7 +96,6 @@ struct smenuitem {
 #define CirkPumpBus 6
 #define ShuntDirBus 5
 #define ShuntBus 4
-
 #define ScreentimeOutSec 10
 #define ButtontimeOutSec 5
 #define ROFF HIGH
@@ -129,6 +123,7 @@ float val2=0;
 int currview=0;
 int currOutSideTemp=0;
 int currPipeTemp=0;
+int currCalcTemp=0;
 int secLeft=0;
 int secRight=0;
 LiquidCrystal_I2C lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
@@ -141,57 +136,36 @@ int ports[pc][4] = {
   {8, 0, 0, 0} ,
   {9, 0, 0, 0} };
 const int Chnums=21;
-
-
-
- smenuitem menuItems[menuitemCount];
-
-  bool inMainView=false;
-  bool insubmenu=false;
-  int currentmenuitem=0;
-  int currentsubmenuitem=0;
-  int submenuItemscount=0;
-
+smenuitem menuItems[menuitemCount];
+bool inMainView=false;
+bool insubmenu=false;
+int currentmenuitem=0;
+int currentsubmenuitem=0;
+int submenuItemscount=0;
 bool ERROR=false;
 bool firstrun=true;
 int pdelay=200;
 void setup()
 {
-defMenu();
-pinMode(ShuntBus, OUTPUT);
-pinMode(ShuntDirBus, OUTPUT);
-pinMode(CirkPumpBus, OUTPUT);
-digitalWrite(ShuntBus, ROFF);
-digitalWrite(ShuntDirBus, ROFF);
-digitalWrite(CirkPumpBus, ROFF);
-
-Serial.begin(9600);
-lcd.begin (20,4);
-lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
-lcd.setBacklight(HIGH);
-/*
-lcd.clear() ; // go home on LCD
-char st[Chnums]="####################";
-lcd.setCursor(0, 0);
-lcd.print(st);
-lcd.setCursor(0, 1);
-lcd.print("# Utvecklad 2017   #");
-lcd.setCursor(0, 2);
-lcd.print("# Johan Hahne      #");
-lcd.setCursor(0, 3);
-lcd.print(st);
-*/
-
-//lcd.print("");
-
-for(int i = 0;i<pc;i++){
-pinMode(ports[i][0], OUTPUT);
-digitalWrite(ports[i][0],ROFF);}
-buttoninactive=millis();
-gendel=millis();
-SetCurrTemp();
-  //discoverOneWireDevices();
-  MainView();
+    defMenu();
+    pinMode(ShuntBus, OUTPUT);
+    pinMode(ShuntDirBus, OUTPUT);
+    pinMode(CirkPumpBus, OUTPUT);
+    digitalWrite(ShuntBus, ROFF);
+    digitalWrite(ShuntDirBus, ROFF);
+    digitalWrite(CirkPumpBus, ROFF);
+    Serial.begin(9600);
+    lcd.begin (20,4);
+    lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
+    lcd.setBacklight(HIGH);
+    for(int i = 0;i<pc;i++){
+    pinMode(ports[i][0], OUTPUT);
+    digitalWrite(ports[i][0],ROFF);}
+    buttoninactive=millis();
+    gendel=millis();
+    SetCurrTemp();
+      //discoverOneWireDevices();
+      MainView();
 }
 
 
@@ -217,7 +191,6 @@ if ( millis() - buttoninactive >= ButtontimeOutSec*1000 ){
             errorView();
             }
     }
-    //buttoninactive=millis();
 }
   if ( millis() - ScreentimeOut >= ScreentimeOutSec * 1000 ){
       if (!insubmenu){
@@ -226,10 +199,8 @@ if ( millis() - buttoninactive >= ButtontimeOutSec*1000 ){
             } else {
                   lcd.setBacklight(HIGH);
              }
-
       }
   }
-
   }
   if ( millis() - ShuntrunSec >= menuItems[4].value *1000){
     runshunt(false,true);
@@ -257,9 +228,9 @@ void defMenu(){
     menuItems[m].smenitem.submenuStringvalues[0]="Ja";
     menuItems[m].smenitem.submenuStringvalues[1]="Nej";
     if( EEPROM.read(m) > menuItems[m].smenitem.maxvalue){
-    menuItems[m].value= menuItems[m].smenitem.minvalue;
-    } else{
-    menuItems[m].value= EEPROM.read(m);
+        menuItems[m].value= menuItems[m].smenitem.minvalue;
+    } else {
+        menuItems[m].value= EEPROM.read(m);
     }
     //meny2
     m =1;
@@ -270,23 +241,21 @@ void defMenu(){
     menuItems[m].smenitem.submenuStringvalues[0]="";
     menuItems[m].smenitem.submenuStringvalues[1]="";
     if( EEPROM.read(m) > menuItems[m].smenitem.maxvalue){
-    menuItems[m].value= menuItems[m].smenitem.minvalue;
-    } else{
-    menuItems[m].value= EEPROM.read(m);
+        menuItems[m].value= menuItems[m].smenitem.minvalue;
+    } else {
+        menuItems[m].value= EEPROM.read(m);
     }
     //meny3
     m =2;
-
     menuItems[m].useStringvalue=false;
     menuItems[m].smenitem.maxvalue=30;
     menuItems[m].smenitem.minvalue=0;
-
     menuItems[m].smenitem.submenuStringvalues[0]="";
     menuItems[m].smenitem.submenuStringvalues[1]="";
     if( EEPROM.read(m) > menuItems[m].smenitem.maxvalue){
-    menuItems[m].value= menuItems[m].smenitem.minvalue;
-    } else{
-    menuItems[m].value= EEPROM.read(m);
+        menuItems[m].value= menuItems[m].smenitem.minvalue;
+    } else {
+        menuItems[m].value= EEPROM.read(m);
     }
     m =3;
     menuItems[m].useStringvalue=false;
@@ -295,43 +264,24 @@ void defMenu(){
     menuItems[m].smenitem.submenuStringvalues[0]="";
     menuItems[m].smenitem.submenuStringvalues[1]="";
     if( EEPROM.read(m) > menuItems[m].smenitem.maxvalue){
-    menuItems[m].value= menuItems[m].smenitem.minvalue;
-    } else{
-    menuItems[m].value= EEPROM.read(m);
+        menuItems[m].value= menuItems[m].smenitem.minvalue;
+    } else {
+        menuItems[m].value= EEPROM.read(m);
     }
     m=4;
-
     menuItems[m].useStringvalue=false;
     menuItems[m].smenitem.maxvalue=10;
     menuItems[m].smenitem.minvalue=1;
     menuItems[m].callFunc=false;
     menuItems[m].hasSubMenu=true;
-
     menuItems[m].smenitem.submenuStringvalues[0]="";
     menuItems[m].smenitem.submenuStringvalues[1]="";
     if( EEPROM.read(m) > menuItems[m].smenitem.maxvalue){
-    menuItems[m].value= menuItems[m].smenitem.minvalue;
-    } else{
-    menuItems[m].value= EEPROM.read(m);
-    }
-
-
-}
-/*
-void char *arr,String str){
-
-for (int i =0;i<Chnums;i++){
-    if(i > str.length()){
-        arr[i]='\0';
-        break;
+        menuItems[m].value= menuItems[m].smenitem.minvalue;
     } else {
-        arr[i]=str[i];
+        menuItems[m].value= EEPROM.read(m);
     }
-
 }
-return str;
-}
-*/
 
 void runshunt(bool right,bool off){
   if (off){
@@ -341,37 +291,30 @@ void runshunt(bool right,bool off){
   } else {
     if (right){
       if (secRight < menuItems[3].value+menuItems[4].value){
-
-    digitalWrite(ShuntDirBus, RON);
-    digitalWrite(ShuntBus, RON);
-    secRight=secRight+menuItems[4].value;
-    ShuntrunSec=millis();
+        digitalWrite(ShuntDirBus, RON);
+        digitalWrite(ShuntBus, RON);
+        secRight=secRight+menuItems[4].value;
+        ShuntrunSec=millis();
     }
     secLeft=0;
     } else {
       if (secLeft < menuItems[3].value+menuItems[4].value){
-
       digitalWrite(ShuntDirBus, ROFF);
       digitalWrite(ShuntBus, RON);
       secLeft=secLeft+menuItems[4].value;
       ShuntrunSec=millis();
     }
       secRight=0;
-
     }
   }
-
-
 }
 
 
 void errorView()
 {
-
-//    lcd.clear();
     char framledning[] ="Framledning:";
     char utegivare[] ="Utegivare: ";
-    char outstr[] ="*FEL*TEMPGIVARE*FEL*";
+    char outstring[] ="*FEL*TEMPGIVARE*FEL*";
     char outtemp[4];
     char intemp[4];
     String s;
@@ -379,60 +322,51 @@ void errorView()
     s.toCharArray(outtemp,4);
     s=(String)currPipeTemp;
     s.toCharArray(intemp,4);
-
-    Printstring(0,0,outstr);
+    Printstring(0,0,outstring);
     if (currOutSideTemp < -126){
-    memset(outstr, 0, sizeof(outstr));
-    strcpy(outstr,utegivare);
-    strcat(outstr, " FEL");
-
+        memset(outstring, 0, sizeof(outstring));
+        strcpy(outstring,utegivare);
+        strcat(outstring, " FEL");
     } else {
-        memset(outstr, 0, sizeof(outstr));
-        strcpy(outstr,utegivare);
-        strcat(outstr,outtemp);
-        strcat(outstr,";C");
-
-
+        memset(outstring, 0, sizeof(outstring));
+        strcpy(outstring,utegivare);
+        strcat(outstring,outtemp);
+        strcat(outstring,";C");
     }
-    Printstring(1,0,outstr );
+    Printstring(1,0,outstring );
      if (currPipeTemp < -126){
-         memset(outstr, 0, sizeof(outstr));
-         strcpy(outstr,framledning);
-         strcat(outstr, " FEL");
-
+         memset(outstring, 0, sizeof(outstring));
+         strcpy(outstring,framledning);
+         strcat(outstring, " FEL");
      } else {
-         memset(outstr, 0, sizeof(outstr));
-         strcpy(outstr,framledning);
-         strcat(outstr,intemp);
-         strcat(outstr,";C");
+         memset(outstring, 0, sizeof(outstring));
+         strcpy(outstring,framledning);
+         strcat(outstring,intemp);
+         strcat(outstring,";C");
      }
-    Printstring(2,0,outstr);
-    memset(outstr, 0, sizeof(outstr));
-    strcpy(outstr,"Ger ut MAX temp! ");
-    Printstring(3,0,outstr);
-
+    Printstring(2,0,outstring);
+    memset(outstring, 0, sizeof(outstring));
+    strcpy(outstring,"Ger ut MAX temp! ");
+    Printstring(3,0,outstring);
 }
-
-
 
 void MainView()
 {
     lcd.clear();
-    String outstr ="   Temperaturer:";
+    outstr ="   Temperaturer:";
     Printstring(0,0,outstr);
     outstr ="Ute: "+(String)currOutSideTemp+";C";
     Printstring(1,0,outstr);
     outstr ="Framledning: "+(String)currPipeTemp+";C";
     Printstring(2,0,outstr);
-
 }
+
 void TempView()
 {
-    //lcd.clear();
+    if(!inMainView){lcd.clear();};
     inMainView=true;
-
     SetCurrTemp();
-    String outstr ="Ute:"+(String)currOutSideTemp+";C :Fr"+(String)currPipeTemp+";C";
+    outstr ="Ute:"+(String)currOutSideTemp+";C :Fram"+(String)currPipeTemp+";C";
     Printstring(0,0,outstr);
      outstr ="20;:"+(String)(int)calcTemp(20)+"; I "+"-10;:"+(String)(int)calcTemp(-10)+";";
     Printstring(1,0,outstr);
@@ -440,50 +374,51 @@ void TempView()
     Printstring(2,0,outstr);
     outstr =" 0;:"+(String)(int)calcTemp(0)+"; I "+"-30;:"+(String)(int)calcTemp(-30)+";";
     Printstring(3,0,outstr);
-
-
-/*
-    String outstr ="U/F 20/"+(String)(int)calcTemp(20)+" 10/"+(String)(int)calcTemp(10)+" 0/"+(String)(int)calcTemp(0);
-
-
-     +" -20/"+(String)(int)calcTemp(-20)+" -30/"+(String)(int)calcTemp(-30);
-    Printstring(1,0,outstr);
-     outstr ="F1: "+(String)factor1 + "   F2: "+(String)factor2;
-    Printstring(3,0,outstr);
-    //outstr ="";
-    //Printstring(3,0,outstr);
-*/
 }
+
+void AdjustView(bool upp)
+{
+    if(inMainView){
+        lcd.clear();
+        if (upp){
+            outstr ="   JUSTERAR UPP     ";
+        } else {
+            outstr ="   JUSTERAR NER     ";
+        }
+        Printstring(0,0,outstr);
+        outstr =emptyRow;
+        Printstring(1,0,outstr);
+        outstr ="Fram"+(String)currPipeTemp+";C -> "+(String)currCalcTemp+";C";
+        Printstring(2,0,outstr);
+        outstr =emptyRow;
+        Printstring(3,0,outstr);
+    }
+}
+
 void Printstring(int row,int index,String outstring)
 {
     lcd.setCursor(index, row);
     int c = Chnums-1;
     for (int i = 0; i<c;i++)
     {
-        if (outstring[i] != 0)
-        {
-        if (outstring[i] == ';')
-        {
-            lcd.print((char)223);
-            //Serial.print((char)223);
-        } else {
-            if (outstring[i] == '@'){
-                if (menuItems[currentmenuitem].useStringvalue){
-                    lcd.print(menuItems[currentmenuitem].smenitem.submenuStringvalues[menuItems[currentmenuitem].value]);
-            //        Serial.print(menuItems[currentmenuitem].smenitem.submenuStringvalues[menuItems[currentmenuitem].value]);
-            //        Serial.print((String)menuItems[currentmenuitem].value);
-                } else {
-                    lcd.print(menuItems[currentmenuitem].value);
-            //        Serial.print((String)menuItems[currentmenuitem].value);
-                }
+        if (outstring[i] != 0) {
+            if (outstring[i] == ';')
+            {
+                lcd.print((char)223);
+                //Serial.print((char)223);
             } else {
-                lcd.print(outstring[i]);
-            //    Serial.print(outstring[i]);
+                if (outstring[i] == '@'){
+                    if (menuItems[currentmenuitem].useStringvalue){
+                        lcd.print(menuItems[currentmenuitem].smenitem.submenuStringvalues[menuItems[currentmenuitem].value]);
+                    } else {
+                        lcd.print(menuItems[currentmenuitem].value);
+                    }
+                } else {
+                    lcd.print(outstring[i]);
+                }
             }
         }
-        }
     }
-    //Serial.println();
   }
 
 void setFactors()
@@ -494,10 +429,7 @@ void setFactors()
         ScreentimeOut=millis();
         lcd.setBacklight(HIGH);
     }
-
     lastfactor = (int)(val+val2);
-
-
 }
 
 float calcTemp(int outTemp)
@@ -508,20 +440,17 @@ float v =0;
 c=c*factor1;
 for(int i=20;i > -31; i-- ){
     if(i==outTemp){
-        v=(c*factor2);//*factor2;
-
-            //v=(c*factor1)*factor2;
+        v=(c*factor2);
     break;
     }
 
   c++;
 }
 if(v > 99){
-return 99;
-} else {
-return v;
-}
-
+        return 99;
+    } else {
+        return v;
+    }
 }
 
 void readButtons() {
@@ -541,13 +470,9 @@ for(int i = 0;i<pc;i++){
       }
       if(ports[i][1]==1){
            ports[i][2]=1;
-
           buttonpush(i);
           buttoninactive=millis();
-          //Serial.print(buttoninactive);
-    }
-
-
+      }
     }
 }
 btnDelay=millis();
@@ -585,7 +510,7 @@ void Prevbutton(){
   } else {
     currentsubmenuitem--;
   }
-showsubmenuitem(currentmenuitem,currentsubmenuitem);
+  showsubmenuitem(currentmenuitem,currentsubmenuitem);
   }
 }
 
@@ -635,19 +560,16 @@ PrintMenuOnScreen(false);
 }
 
 void showsubmenuitem(int menuitem,int submenuitem){
-if (menuItems[currentmenuitem].hasSubMenu){
-    insubmenu=true;
-    lcd.clear();
-    menuItems[currentmenuitem].value=submenuitem;
-    PrintMenuOnScreen(true);
-
-}
-
+    if (menuItems[currentmenuitem].hasSubMenu){
+        insubmenu=true;
+        lcd.clear();
+        menuItems[currentmenuitem].value=submenuitem;
+        PrintMenuOnScreen(true);
+    }
 }
 
 
 void PrintMenuOnScreen(bool submenu){
-
 for(int row=0;row<4;row++){
     memset(buffer,0,sizeof(buffer));
     if(!submenu){
@@ -687,38 +609,6 @@ for(int row=0;row<4;row++){
     }
 }
 
-
-
-/*
-void discoverOneWireDevices(void) {
-  byte i;
-  byte present = 0;
-  byte data[12];
-  byte addr[8];
-
-  Serial.print("Looking for 1-Wire devices...\n\r");
-  while(ds.search(addr)) {
-    Serial.print("\n\rFound \'1-Wire\' device with address:\n\r");
-    for( i = 0; i < 8; i++) {
-      Serial.print("0x");
-      if (addr[i] < 16) {
-        Serial.print('0');
-      }
-      Serial.print(addr[i], HEX);
-      if (i < 7) {
-        Serial.print(", ");
-      }
-    }
-    if ( OneWire::crc8( addr, 7) != addr[7]) {
-        Serial.print("CRC is not valid!\n");
-        return;
-    }
-  }
-  Serial.print("\n\r\n\rThat's it.\r\n");
-  ds.reset_search();
-  return;
-}
-*/
 void SetCurrTemp(){
 TempOutSide.requestTemperatures();
 currOutSideTemp=(int)TempOutSide.getTempCByIndex(0);
@@ -731,22 +621,24 @@ if ((currOutSideTemp > -126 )&&(currPipeTemp > -126)){
     ERROR=true;
 
 }
-
 }
 
 void calcTempAndRunShunt(){
 SetCurrTemp();
 ShunttimeOut=millis();
-if(!ERROR){
+    if(!ERROR){
         int aTemp = (int)calcTemp(currOutSideTemp);
-            if (currPipeTemp < aTemp){
+        currCalcTemp=aTemp;
+            if (currPipeTemp < aTemp-1){
+              AdjustView(true);
               runshunt(false,false);
             }
-        if (currPipeTemp > aTemp){
+        if (currPipeTemp > aTemp+1){
+              AdjustView(false);
               runshunt(true,false);
             }
-        } else {
+    } else {
         runshunt(false,false);
-       }
 
+    }
 }
